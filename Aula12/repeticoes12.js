1a724b311c620ddc4419bbafebf7cf5e9fa34996
Hebert Santos <hebertsantos@Air-de-Hebert.home>
2021-06-17 14:00:06 -0300

    while, do-while, for

---
+++ Aula12/repeticoes12.js
@@ -0,0 +1,22 @@
+/* Estrutura de repetições */
+
+
+var contador = 1
+    while (contador < 10) {
+        console.log('tudo bem')
+        contador++
+    }
+
+/* ou */
+
+
+do {
+    console.log('tudo bem')
+    contador++
+} while (contador <= 2)
+
+/* OU */
+
+for ( var contador = 1 ; contador <= 10 ; contador++ ) {
+    console.log(contador)
+}
